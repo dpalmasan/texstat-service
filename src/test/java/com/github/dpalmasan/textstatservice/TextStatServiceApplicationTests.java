c6e1490951c13ed5b98f415961884d33bd75f930
dpalmasan <dpalmasan@gmail.com>
2022-02-17 18:27:13 -0300

    Fix typo on app package name (#1)

--- src/test/java/com/github/dpalmasan/textstatservice/TextStatServiceApplicationTests.java
+++ src/test/java/com/github/dpalmasan/textstatservice/TextStatServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.dpalmasan.TextStatService;
+package com.github.dpalmasan.textstatservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TextStatServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
